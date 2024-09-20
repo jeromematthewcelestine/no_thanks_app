ed265037f390a50d2d1221d93646ff423346f8cf
Jerome Williams <jeromematthewcelestine@gmail.com>
2024-09-19 21:19:03 -0400

    Additional edits.

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,78 @@
 "use client"
 
-// import Image from "next/image";
 import NoThanksBoard from "./NoThanksBoard";
+import { mcts } from "./mcts";
 import { NoThanksGame, NoThanksState } from "./NoThanks";
-// import { mcts } from "./mcts";
 import { useEffect, useState } from "react";
 
 export default function App() {
-
-  const game = new NoThanksGame();
+  const game = new NoThanksGame(
+    3,
+    20,
+    3,
+    6,
+    4
+  );
   const [gameState, setGameState] = useState<NoThanksState>();
+  const [humanPlayer, setHumanPlayer] = useState(0);
+  const [playerNames, setPlayerNames] = useState(["Player", "Alice", "Bob"]);
+  const [isBotTurn, setIsBotTurn] = useState(false);
 
   useEffect(() => {
     setGameState(new NoThanksState(game));
   }, []);
 
+  useEffect(() => {
+    // console.log("gameState or isBotTurn changed");
+    // console.log("isBotTurn", isBotTurn);
+    if (gameState?.currentPlayer !== humanPlayer && !gameState?.isGameOver()) {
+      handleBotTurn();
+    }
+  }, [gameState]);
+
+  const handleBotTurn = async () => {
+    console.log("handleBotTurn");
+
+    if (!gameState)
+      return;
+
+    await new Promise(r => setTimeout(r, 50));
+
+    const botAction = mcts(gameState, 1, true);
+
+    const newGameState = gameState.clone();
+    newGameState.applyAction(botAction);
+    setGameState(newGameState);
+
+    // setGameState(prevState => {
+    //   const newState = prevState?.clone();
+    //   newState?.applyAction(botAction);
+    //   return newState;
+    // });
+
+    setIsBotTurn(newGameState.getCurrentPlayer() !== humanPlayer);
+    // setIsBotTurn(prevIsBotTurn => {
+    //   const currentPlayer = gameState.getCurrentPlayer();
+    //   return currentPlayer !== humanPlayer && !gameState.isGameOver();
+    // });
+  }
+
   const handleAction = (action: number) => {
     if (!gameState)
       return;
 
+    const newGameState = gameState.clone();
     if (action === NoThanksState.ACTION_TAKE) {
-      const newGameState = gameState.clone();
       newGameState.applyAction(NoThanksState.ACTION_TAKE);
       setGameState(newGameState);
-      console.log(newGameState.toString());
     } else if (action === NoThanksState.ACTION_PASS) {
-      const newGameState = gameState.clone();
       newGameState.applyAction(NoThanksState.ACTION_PASS);
       setGameState(newGameState);
-      console.log(newGameState.toString());
     }
-    
+
+    if (newGameState.getCurrentPlayer() !== humanPlayer) {
+      setIsBotTurn(true);
+    }
   }
 
   return (
@@ -39,7 +81,12 @@ export default function App() {
         NoThanksBot
       </div>
       <div>
-        {gameState && <NoThanksBoard gameState={gameState} actionHandler={handleAction} />}
+        {gameState && 
+        <NoThanksBoard 
+          gameState={gameState} 
+          humanPlayer={humanPlayer} 
+          playerNames={playerNames}
+          actionHandler={handleAction} />}
       </div>
     </div>
   );
